d85eb0ff796c488664521d003c97a2bb5089e890
lekhweemeng <lek.hweemeng@live.com>
2020-10-12 18:16:48 +0800

    updated projects page

--- app/javascript/components/projects.jsx
+++ app/javascript/components/projects.jsx
@@ -11,13 +11,45 @@ class Projects extends React.Component {
     render(){
         return (
             <div id="projectsx" className="projects">
-                <figure>
-                    <img src="http://lorempixel.com/192/96/sports/1" alt="Portfolio Item" />
-                    <figcaption>
-                        <h3>Title</h3>
-                        <p>Description.</p>
-                    </figcaption>
-                </figure>
+                <div className="projects-holder">
+                    <h3 className="centering">Project Portfolios</h3>
+                    <a className="projects-indv" href="http://www.google.com">
+                        <figure>
+                            <img src="http://lorempixel.com/192/96/sports/1" alt="Portfolio Item" />
+                            <figcaption>
+                                <h3>Title</h3>
+                                <p>Description.</p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a className="projects-indv"href="http://www.google.com">
+                        <figure>
+                            <img src="http://lorempixel.com/192/96/sports/1" alt="Portfolio Item" />
+                            <figcaption>
+                                <h3>Title</h3>
+                                <p>Description.</p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a className="projects-indv"href="http://www.google.com">
+                        <figure>
+                            <img src="http://lorempixel.com/192/96/sports/1" alt="Portfolio Item" />
+                            <figcaption>
+                                <h3>Title</h3>
+                                <p>Description.</p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a className="projects-indv"href="http://www.google.com">
+                        <figure>
+                            <img src="http://lorempixel.com/192/96/sports/1" alt="Portfolio Item" />
+                            <figcaption>
+                                <h3>Title</h3>
+                                <p>Description.</p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </div>
             </div>
             );
     }
