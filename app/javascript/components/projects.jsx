c2ec1c4bbe1662697839bb4184369ea96200f067
lekhweemeng <lek.hweemeng@live.com>
2020-10-21 11:34:26 +0800

    done some minor tweak

--- app/javascript/components/projects.jsx
+++ app/javascript/components/projects.jsx
@@ -28,53 +28,100 @@ class Projects extends React.Component {
                     <figure>
                         <img src={ProjectOne} alt="Portfolio Item" />
                         <figcaption>
-                        <a className="projects-indv" href="https://hweemeng.github.io/project-1/"><p className="projectHeader">Hwee Meng's Dungeon Game</p></a>
-                        <a href='https://github.com/HweeMeng' className='navButts'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
-                                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                            </svg>
+                        <p className="projectHeader">Mini Dungeon Game</p>
+                        <a href='https://hweemeng.github.io/project-1/' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in">
+                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                            <polyline points="10 17 15 12 10 7"></polyline>
+                            <line x1="15" y1="12" x2="3" y2="12"></line>    
+                        </svg>
                         </a>
-                            <span>
-                                <p>This is a dungeon RPG game created on Javascript and DOM Manipulation. It is hosted in Githib Pages</p>
-                                <img className="logos" src={GithubPages}/>
-                            </span>
+                        <a href='https://github.com/HweeMeng/project-1' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                            </path>
+                        </svg>
+                        </a>
+                        <span>
+                            <p>This is a dungeon RPG game created on Javascript and DOM Manipulation. It is hosted in Githib Pages</p>
+                            <img className="logos" src={GithubPages}/>
+                        </span>
                         </figcaption>
                     </figure>
                     <figure>
                         <img src={goldFishTrainerds} alt="Portfolio Item" />
                         <figcaption>
-                        <a className="projects-indv"href="https://goldfish-trainerds.herokuapp.com/"><p className="projectHeader">Goldfish Trainerds</p></a>
-                            <span>
-                                <p>This is a Group Project. It is built on Ruby on Rails, with Postgres SQL as Database and hosted on Heroku. This is a Social media application for goldifsh lovers</p>
-                                <img className="logos" src={Rails}/>&nbsp;
-                                <img className="logos" src={Postgres}/>&nbsp;
-                                <img className="logos" src={HerokuLogo}/>
-                            </span>
+                        <p className="projectHeader">Goldfish Trainerds</p>
+                        <a href='https://goldfish-trainerds.herokuapp.com/' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in">
+                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                            <polyline points="10 17 15 12 10 7"></polyline>
+                            <line x1="15" y1="12" x2="3" y2="12"></line>    
+                        </svg>
+                        </a>
+                        <a href='https://github.com/kwansing14/Goldfish-Training-Project' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                            </path>
+                        </svg>
+                        </a>
+                        <span>
+                            <p>This is a Group Project. It is built on Ruby on Rails, with Postgres SQL as Database and hosted on Heroku. This is a Social media application for goldifsh lovers</p>
+                            <img className="logos" src={Rails}/>&nbsp;
+                            <img className="logos" src={Postgres}/>&nbsp;
+                            <img className="logos" src={HerokuLogo}/>
+                        </span>
                         </figcaption>
                     </figure>
                     <figure>
                         <img src={Travinary} alt="Portfolio Item" />
                         <figcaption>
-                        <a className="projects-indv"href="https://travinary.herokuapp.com/"><p className="projectHeader">Travinary</p></a>
-                            <span>
-                                <p>This is a Itinery Planning app for users to enter the country and location which they would like to visit.</p>
-                                <img className="logos" src={Rails}/>&nbsp;
-                                <img className="logos" src={Postgres}/>&nbsp;
-                                <img className="logos" src={ReactIcon}/>&nbsp;
-                                <img className="logos" src={HerokuLogo}/>
-                            </span>
+                        <p className="projectHeader">Travinary</p>
+                        <a href='https://travinary.herokuapp.com/' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in">
+                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                            <polyline points="10 17 15 12 10 7"></polyline>
+                            <line x1="15" y1="12" x2="3" y2="12"></line>    
+                        </svg>
+                        </a>
+                        <a href='https://github.com/HweeMeng/project-4' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                            </path>
+                        </svg>
+                        </a>
+                        <span>
+                            <p>This is a Itinery Planning app for users to enter the country and location which they would like to visit.</p>
+                            <img className="logos" src={Rails}/>&nbsp;
+                            <img className="logos" src={Postgres}/>&nbsp;
+                            <img className="logos" src={ReactIcon}/>&nbsp;
+                            <img className="logos" src={HerokuLogo}/>
+                        </span>
                         </figcaption>
                     </figure>
                     <figure>
                         <img src={meetUpDrf} alt="Portfolio Item" />
                         <figcaption>
-                        <a className="projects-indv"href="http://meetup-env.eba-ynfrzqwv.ap-southeast-1.elasticbeanstalk.com/api/meetups/"><p className="projectHeader">Meetup DRF</p></a>
-                            <span>
-                                <p>This is a backend API project. It is built on Python with Django on SQLite3 Database. It is meant to serve a React Front end</p>
-                                <img className="logos" src={ElasticLogo}/>&nbsp;
-                                <img className="logos" src={DjangoLogo}/>&nbsp;
-                                <img className="logos" src={SQLite3Logo}/>
-                            </span> 
+                        <p className="projectHeader">Meetup DRF</p>
+                        <a href='http://meetup-env.eba-ynfrzqwv.ap-southeast-1.elasticbeanstalk.com/api/meetups/' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in">
+                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                            <polyline points="10 17 15 12 10 7"></polyline>
+                            <line x1="15" y1="12" x2="3" y2="12"></line>    
+                        </svg>
+                        </a>
+                        <a href='https://github.com/HweeMeng/meetups' className='goToAndGit'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                            </path>
+                        </svg>
+                        </a>
+                        <span>
+                            <p>This is a backend API project. It is built on Python with Django on SQLite3 Database. It is meant to serve a React Front end</p>
+                            <img className="logos" src={ElasticLogo}/>&nbsp;
+                            <img className="logos" src={DjangoLogo}/>&nbsp;
+                            <img className="logos" src={SQLite3Logo}/>
+                        </span> 
                         </figcaption>
                     </figure>
                 </div>
