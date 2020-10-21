15f5672e0328809bb55eccfd2bb6615bbcd0eec6
lekhweemeng <lek.hweemeng@live.com>
2020-10-21 10:55:00 +0800

    updated some styling

--- app/javascript/components/landing.jsx
+++ app/javascript/components/landing.jsx
@@ -16,7 +16,7 @@ class Landing extends React.Component {
                     <p>Hi, My Name is</p>
                     <p className="bigCaption1">Lek Hwee Meng</p>
                     <p className="bigCaption2">I build applications for the web.</p>
-                    <p className="slightlyBigger">I am a Full-stack Web Developer based in Singapore. I specialize in building web applications with Rails and Django on React.</p>
+                    <p className="slightlyBigger">I am a Full-stack Web Developer based in Singapore. <br/> I specialize in building web applications with Rails and Django on React.</p>
                 </div>
             </div>
             );
